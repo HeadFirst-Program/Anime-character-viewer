4df04193c55c760e10b5ce73379ad05132df5a5c
Doring <d0211468@kowon.dongseo.ac.kr>
2021-07-04 00:12:22 +0900

    API
    API제어 및 실행과 관련된 함수가 있는 곳입니다.

---
+++ ver 1/com_api.py
@@ -0,0 +1,493 @@
+import pip._vendor.requests as requests
+import com_api as api
+import os
+import time
+
+chrome_run_command = 'start chrome --incognito ' #크롬 시크릿모드 실행 명령어
+browser_sleep_time = 1 # 웹브라우저가 제대로 실행되기 위한 여분의 시간이자 다음 사진이 나타나기까지의 시간
+
+
+#by VDoring. 2021.07.03
+#sfw type에 속한 category를 실행하는 함수들입니다.
+#리턴값: 없음
+
+def sfwWaifu():
+    res = requests.get('https://api.waifu.pics/sfw/waifu')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwNeko():
+    res = requests.get('https://api.waifu.pics/sfw/neko')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwShinobu():
+    res = requests.get('https://api.waifu.pics/sfw/shinobu')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwMegumin():
+    res = requests.get('https://api.waifu.pics/sfw/megumin')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwBully():
+    res = requests.get('https://api.waifu.pics/sfw/bully')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwCuddle():
+    res = requests.get('https://api.waifu.pics/sfw/cuddle')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwCry():
+    res = requests.get('https://api.waifu.pics/sfw/cry')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwHug():
+    res = requests.get('https://api.waifu.pics/sfw/hug')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwAwoo():
+    res = requests.get('https://api.waifu.pics/sfw/awoo')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwKiss():
+    res = requests.get('https://api.waifu.pics/sfw/kiss')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwLick():
+    res = requests.get('https://api.waifu.pics/sfw/lick')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwPat():
+    res = requests.get('https://api.waifu.pics/sfw/pat')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwSmug():
+    res = requests.get('https://api.waifu.pics/sfw/smug')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwBonk():
+    res = requests.get('https://api.waifu.pics/sfw/bonk')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwYeet():
+    res = requests.get('https://api.waifu.pics/sfw/yeet')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwBlush():
+    res = requests.get('https://api.waifu.pics/sfw/blush')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwSmile():
+    res = requests.get('https://api.waifu.pics/sfw/smile')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwWave():
+    res = requests.get('https://api.waifu.pics/sfw/wave')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwHighfive():
+    res = requests.get('https://api.waifu.pics/sfw/highfive')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwHandhold():
+    res = requests.get('https://api.waifu.pics/sfw/handhold')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwNom():
+    res = requests.get('https://api.waifu.pics/sfw/nom')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwBite():
+    res = requests.get('https://api.waifu.pics/sfw/bite')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwGlomp():
+    res = requests.get('https://api.waifu.pics/sfw/glomp')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwSlap():
+    res = requests.get('https://api.waifu.pics/sfw/slap')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwKill():
+    res = requests.get('https://api.waifu.pics/sfw/kill')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwKick():
+    res = requests.get('https://api.waifu.pics/sfw/kick')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwHappy():
+    res = requests.get('https://api.waifu.pics/sfw/happy')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwWink():
+    res = requests.get('https://api.waifu.pics/sfw/wink')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwPoke():
+    res = requests.get('https://api.waifu.pics/sfw/poke')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwDance():
+    res = requests.get('https://api.waifu.pics/sfw/dance')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def sfwCringe():
+    res = requests.get('https://api.waifu.pics/sfw/cringe')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+
+#by VDoring. 2021.07.03
+#nsfw type에 속한 category를 실행하는 함수들입니다.
+#리턴값: 없음
+
+def nsfwWaifu():
+    res = requests.get('https://api.waifu.pics/nsfw/waifu')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def nsfwNeko():
+    res = requests.get('https://api.waifu.pics/nsfw/neko')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def nsfwTrap():
+    res = requests.get('https://api.waifu.pics/nsfw/trap')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+def nsfwBlowjob():
+    res = requests.get('https://api.waifu.pics/nsfw/blowjob')
+    link = res.text
+    os.system(chrome_run_command + link[8:-3])
+    time.sleep(browser_sleep_time)
+
+
+#by VDoring. 2021.07.03
+#예외처리를 포함하여 category를 재생합니다.
+#매개변수: category_name은 string이고, 카테코리 이름을 받습니다.
+#리턴값: 없음
+def sfwPlay(category_name):
+    os.system('mode con cols=40 lines=11') # 화면 크기 설정
+    os.system('title Setting..') # 윈도우타이틀 설정
+
+    print('\nCurrent delay time: %d seconds\n'%browser_sleep_time)
+    print('You want change delay time? [y/n]')
+    act = input('= ')
+    if act.lower() == 'y':
+        delayTimeSet()
+
+    os.system('title Playing..') # 윈도우타이틀 설정
+    print('\nif you want to stop, press Ctrl+C')
+    
+    if category_name == 'waifu':
+        try:
+            while True:
+                api.sfwWaifu()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'neko':
+        try:
+            while True:
+                api.sfwNeko()
+        except KeyboardInterrupt:
+            return
+
+    elif category_name == 'megumin':
+        try:
+            while True:
+                api.sfwMegumin()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'bully':
+        try:
+            while True:
+                api.sfwBully()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'cuddle':
+        try:
+            while True:
+                api.sfwCuddle()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'cry':
+        try:
+            while True:
+                api.sfwCry()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'hug':
+        try:
+            while True:
+                api.sfwHug()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'awoo':
+        try:
+            while True:
+                api.sfwAwoo()
+        except KeyboardInterrupt:
+            return
+
+    elif category_name == 'kiss':
+        try:
+            while True:
+                api.sfwKiss()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'lick':
+        try:
+            while True:
+                api.sfwLick()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'pat':
+        try:
+            while True:
+                api.sfwPat()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'smug':
+        try:
+            while True:
+                api.sfwSmug()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'bonk':
+        try:
+            while True:
+                api.sfwBonk()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'yeet':
+        try:
+            while True:
+                api.sfwYeet()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'blush':
+        try:
+            while True:
+                api.sfwBlush()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'smile':
+        try:
+            while True:
+                api.sfwSmile()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'wave':
+        try:
+            while True:
+                api.sfwWave()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'highfive':
+        try:
+            while True:
+                api.sfwHighfive()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'handhold':
+        try:
+            while True:
+                api.sfwHandhold()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'bite':
+        try:
+            while True:
+                api.sfwBite()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'glomp':
+        try:
+            while True:
+                api.sfwGlomp()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'slap':
+        try:
+            while True:
+                api.sfwSlap()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'kill':
+        try:
+            while True:
+                api.sfwKill()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'kick':
+        try:
+            while True:
+                api.sfwKick()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'happy':
+        try:
+            while True:
+                api.sfwHappy()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'wink':
+        try:
+            while True:
+                api.sfwWink()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'poke':
+        try:
+            while True:
+                api.sfwPoke()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'dance':
+        try:
+            while True:
+                api.sfwDance()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'cringe':
+        try:
+            while True:
+                api.sfwCringe()
+        except KeyboardInterrupt:
+            return
+        
+        
+#by VDoring. 2021.07.03
+#예외처리를 포함하여 category를 재생합니다.
+#매개변수: category_name은 string이고, 카테코리 이름을 받습니다.
+#리턴값: 없음
+def nsfwPlay(category_name):
+    os.system('mode con cols=40 lines=11') # 화면 크기 설정
+    os.system('title Setting..') # 윈도우타이틀 설정
+
+    print('\nCurrent delay time: %d seconds\n'%browser_sleep_time)
+    print('You want change delay time? [y/n]')
+    act = input('= ')
+    if act.lower() == 'y':
+        delayTimeSet()
+
+    os.system('title Playing..') # 윈도우타이틀 설정
+    print('\nif you want to stop, press Ctrl+C')
+    
+    if category_name == 'waifu':
+        try:
+            while True:
+                api.nsfwWaifu()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'neko':
+        try:
+            while True:
+                api.nsfwWaifu()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'trap':
+        try:
+            while True:
+                api.nsfwWaifu()
+        except KeyboardInterrupt:
+            return
+    elif category_name == 'blowjob':
+        try:
+            while True:
+                api.nsfwWaifu()
+        except KeyboardInterrupt:
+            return
+        
+
+#by VDoring. 2021.07.03
+#세부 설정을 다룹니다.(Image delay time)
+#리턴값: 없음
+def delayTimeSet():
+    global browser_sleep_time
+    while True:
+        os.system('mode con cols=50 lines=11') # 화면 크기 설정
+        os.system('title Setting..') # 윈도우타이틀 설정
+
+        print('< Set image delay time >')
+        print('unit of time: Seconds(0, 1, 2, etc..)')
+        print('\nCurrent delay time: %d seconds\n'%browser_sleep_time)
+        sec = input('= ')
+
+        if sec.isdigit(): # 유저 입력값이 숫자로만 이루어진 경우
+            if int(sec) >= 0:
+                browser_sleep_time = int(sec)
+                print('Set %d seconds!'%browser_sleep_time)
+                time.sleep(1)
+                return
+            else: # 유저 입력값이 음수일 때
+                print('[!] Please set over 0! [!]')
+                time.sleep(0.75)
+        else: # 입력값에 문자가 들어갔을 경우
+            print('[!] Please enter a number! [!]')
+            time.sleep(0.75)
