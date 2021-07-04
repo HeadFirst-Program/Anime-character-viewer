0bb8e623562abc82a0c644fbecb2d3bb38506bb9
Doring <d0211468@kowon.dongseo.ac.kr>
2021-07-05 03:29:20 +0900

    main #1
    프로그램 흐름의 중심이 되는 파일입니다.
    
    현재 Single play mode와 Multi play mode 중 하나를 고르는 것, Single play mode와 Settings가 작동됩니다.

---
+++ ver 2/main.py
@@ -0,0 +1,11 @@
+import com_API as api
+import com_CLI as cli
+
+while True:
+    user_input = cli.screenTitle() # Single모드와 Multi모드 중 하나를 고릅니다.
+    if user_input == 1:
+        cli.screenSingleModeSetting() # Single모드 실행 전 환경설정
+    elif user_input == 2:
+        cli.screenMuitiModeSetting() # Muiti모드 실행 전 환경설정
+    elif user_input == 3:
+        cli.screenSettings() # 환경설정 메뉴
