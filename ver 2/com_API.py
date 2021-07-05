9c9cd52247e69a89a73e5ca5d68765a4c2ff0952
Doring <d0211468@kowon.dongseo.ac.kr>
2021-07-05 14:40:27 +0900

    API #2
    API 및 image 출력과 관련된 변수와 함수가 있는 파일입니다.
    
    사용자가 선택한 이미지 type과 category에 맞는 이미지를 연속으로 출력하는 playImageRepeat(user_image_info) 함수 구현.
    사용자가 선택한 이미지 type과 category에 맞는 이미지를 출력하는 playImage(user_image_type, user_image_category) 함수 구현

--- ver 2/com_API.py
+++ ver 2/com_API.py
@@ -11,10 +11,12 @@ is_run_browser_changed = 0
 
 #by VDoring. 2021.07.05
 #com_API.py의 특정 변수의 값을 올립니다.
-#매개변수: name=값을 올릴 변수의 이름
+#매개변수: val_name=값을 올릴 변수의 이름
 #리턴값: 없음
-def valApiCountUp(name):
-    if name == 'browser_changed':
+def valApiCountUp(val_name):
+    global is_run_browser_changed
+
+    if val_name == 'browser_changed':
         is_run_browser_changed += 1
 
 
@@ -38,7 +40,7 @@ def setDelayTime():
             if user_input >= 0:
                 image_delay_time = user_input
                 print('Complete. Now delay time is %f seconds.'%image_delay_time)
-                time.sleep(1.5)
+                time.sleep(1.25)
                 return
         except:
             print('[!] Please enter a number! [!]')
@@ -68,14 +70,55 @@ def setBrowser():
             current_run_browser_name = 'chrome'
             is_run_browser_changed += 1
             print('Complete. Now browser is %s.'%current_run_browser_name.upper())
-            time.sleep(1.5)
+            time.sleep(1.25)
             return
         elif user_input.lower() == 'edge':
             current_run_browser_name = 'edge'
             is_run_browser_changed += 1
             print('Complete. Now browser is %s.'%current_run_browser_name.upper())
-            time.sleep(1.5)
+            time.sleep(1.25)
             return
         else:
             print('[!] Please correct enter a string! [!]')
             time.sleep(0.75)
+
+
+#by VDoring. 2021.07.05
+#사용자가 선택한 이미지 type과 category에 맞는 이미지를 연속으로 출력합니다.
+#매개변수: user_image_info=사용자가 선택한 이미지 type과 category
+#리턴값: 없음
+def playImageRepeat(user_image_info):
+    os.system('mode con cols=40 lines=11')
+    
+    print('< if you want to stop, press Ctrl+C >')
+    try:
+        while True:
+            playImage(user_image_info[:3], user_image_info[3:])
+    except KeyboardInterrupt:
+        return
+
+
+#by VDoring. 2021.07.05
+#사용자가 선택한 이미지 type과 category에 맞는 이미지를 출력합니다.
+#매개변수: user_image_type=사용자가 선택한 이미지 type
+#          user_image_category=사용자가 선택한 이미지 category
+#리턴값: 없음
+def playImage(user_image_type, user_image_category):
+    global image_delay_time
+    global current_run_browser_name
+    global command_chrome_run
+    global command_edge_run
+    
+    image_url = 'https://api.waifu.pics/type/category'
+    image_url = image_url.replace('type', user_image_type.lower())
+    image_url = image_url.replace('category', user_image_category.lower())
+
+    res = requests.get(image_url) # URL 구하는데 0.5초 걸린다
+    image_url = res.text
+    
+    if current_run_browser_name == 'chrome':
+        os.system(command_chrome_run + image_url[8:-3])
+    elif current_run_browser_name == 'edge':
+        os.system(command_edge_run + image_url[8:-3])
+
+    time.sleep(image_delay_time)
