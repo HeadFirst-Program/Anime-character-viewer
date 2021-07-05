ee8ac9b4f9e9f9b6af245f162c3023b1b9197a10
Doring <d0211468@kowon.dongseo.ac.kr>
2021-07-05 19:12:01 +0900

    main #4
    프로그램 작동 흐름의 중심이 되는 파일입니다.
    
    Multi 모드를 실행하기 전 해야하는 셋팅 설정을 완료하였습니다.

--- ver 2/main.py
+++ ver 2/main.py
@@ -7,6 +7,7 @@ while True:
         cli.screenSingleModeSetting() # Single모드 실행 전 환경설정
         api.playImageRepeat(cli.screenCategorySelect(cli.screenTypeSelect())) # type과 category 선택 후 이미지 재생
     elif user_input == 2:
-        cli.screenMuitiModeSetting() # Muiti모드 실행 전 환경설정
+        cli.screenMultiModeSetting() # Multi모드 실행 전 환경설정
+       #cli.screenCategorySelect(cli.screenTypeSelect())
     elif user_input == 3:
         cli.screenSettings() # 환경설정 메뉴
