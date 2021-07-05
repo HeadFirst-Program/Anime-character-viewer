9e40e05a9e33a6d9ebe7617916af536e7882cf4c
Doring <d0211468@kowon.dongseo.ac.kr>
2021-07-05 18:28:59 +0900

    main #3
    프로그램 흐름의 중심이 되는 파일입니다.
    
    Single 모드와 Multi 모드가 독립적으로 움직일 수 있게 코드 구조를 변경하였습니다.

--- ver 2/main.py
+++ ver 2/main.py
@@ -5,10 +5,8 @@ while True:
     user_input = cli.screenTitle() # Single모드와 Multi모드 중 하나를 고릅니다.
     if user_input == 1:
         cli.screenSingleModeSetting() # Single모드 실행 전 환경설정
+        api.playImageRepeat(cli.screenCategorySelect(cli.screenTypeSelect())) # type과 category 선택 후 이미지 재생
     elif user_input == 2:
         cli.screenMuitiModeSetting() # Muiti모드 실행 전 환경설정
     elif user_input == 3:
         cli.screenSettings() # 환경설정 메뉴
-        continue
-    
-    api.playImageRepeat(cli.screenCategorySelect(cli.screenTypeSelect())) # type과 category 선택 후 이미지 재생
