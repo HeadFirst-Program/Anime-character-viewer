9ae76bee14ee0d9c3ab2de17c346bc3bd6da58b7
Doring <d0211468@kowon.dongseo.ac.kr>
2021-07-06 12:08:22 +0900

    main #5
    프로그램 작동 흐름의 중심이 되는 파일입니다.
    
    [2] Multi play mode 기능용 함수 api.playMultiImageRepeat() 추가

--- ver 2/main.py
+++ ver 2/main.py
@@ -5,9 +5,9 @@ while True:
     user_input = cli.screenTitle() # Single모드와 Multi모드 중 하나를 고릅니다.
     if user_input == 1:
         cli.screenSingleModeSetting() # Single모드 실행 전 환경설정
-        api.playImageRepeat(cli.screenCategorySelect(cli.screenTypeSelect())) # type과 category 선택 후 이미지 재생
+        api.playSingleImageRepeat(cli.screenCategorySelect(cli.screenTypeSelect())) # type과 category 선택 후 이미지 재생
     elif user_input == 2:
         cli.screenMultiModeSetting() # Multi모드 실행 전 환경설정
-       #cli.screenCategorySelect(cli.screenTypeSelect())
+        api.playMultiImageRepeat(cli.screenCategorySelect(cli.screenTypeSelect()))
     elif user_input == 3:
         cli.screenSettings() # 환경설정 메뉴
